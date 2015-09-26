695a959bca7efab2d3f72a32197e00fe59abb687
luke <luke@ximbled.co.uk>
2015-09-26 20:03:39 +0100

    [feature] added alerts table migration

---
+++ database/migrations/2015_09_26_175252_create_alerts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alerts', function (Blueprint $table) {
+            $table->increments('id');
+
+            // link on foreign keys
+            $table->integer('candidate_id')->unsigned();
+            $table->foreign('candidate_id')->references('id')->on('candidates')->onDelete('cascade');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('alerts');
+    }
+}
